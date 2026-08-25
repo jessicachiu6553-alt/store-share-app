1fa80af8a0dabb96835a7a6a7f04ceb79bee6a82
wan ka chiu <wankachiu@gmail.com>
2026-08-25 16:59:55 -0400

    favicon and title update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,18 @@
 import { useAuthStore } from '../store/useAuthStore'
 
 const HomePage = () => {
-  const user = useAuthStore((state) => state.user)
+  const user = useAuthStore((state) => state.user);
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
-
   return (
-    <div style={{ textAlign: 'center', marginTop: '80px' }}>
-      <h2>Welcome, {user && isAuthenticated ?  user?.username: "Please Login!!"} 👋</h2>
+    <div style={{ textAlign: "center", marginTop: "80px" }}>
+      <h2>
+        Welcome, {user && isAuthenticated ? user?.username : "Please Login!!"}{" "}
+        👋
+      </h2>
       <p>This is your home page.</p>
     </div>
-  )
+  );
 }
 
 export default HomePage
